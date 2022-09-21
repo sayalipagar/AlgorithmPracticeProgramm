9b3d68e2875a06b1c5c0bde2b15d089c90d1ef8f
sayalipagar <pagarsayali565@gmail.com>
2022-09-21 10:15:39 +0530

    [sayali]Add:BinarySearch

---
+++ AlgorithmProgramm/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgramm
+{
+    internal class BinarySearch
+    {
+        public void Search(string filePath)
+        {
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            Console.WriteLine("****************\n");
+            Console.WriteLine("Enter Words To Search...");
+            string search = Console.ReadLine();
+            foreach (var item in words)
+            {
+                if (search.ToLower().Equals(item.ToLower()))
+                {
+                    Console.WriteLine("Word are Found!!!!!");
+                    Console.WriteLine("************************\n");
+                    return;
+                }
+            }
+            Console.WriteLine("Word Not Found!!!");
+
+        }
+    }
+}
+
--- AlgorithmProgramm/Program.cs
+++ AlgorithmProgramm/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        const string FILE_PATH = @"D:\DotNetPrograms\AlgorithmPracticeProgramm\AlgorithmProgramm\Word.txt";
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome In Algorithm practice Problem!");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Your Choice : \n");
+                Console.WriteLine("1.Binary Search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch search = new BinarySearch();
+                        search.Search(FILE_PATH);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice");
+                        break;
+                }
+            }
         }
     }
 }
