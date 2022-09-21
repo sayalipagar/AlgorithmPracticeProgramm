3a00fe8f779145f6a8d3bc7788ae814dbae4131a
sayalipagar <pagarsayali565@gmail.com>
2022-09-21 10:08:16 +0530

    [sayali]Add:AlgorithmProgramms

---
+++ AlgorithmProgramm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmProgramm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome In Algorithm practice Problem!");
+        }
+    }
+}
