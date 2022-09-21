474f8e38fb33f008911805488750117a99aabc68
sayalipagar <pagarsayali565@gmail.com>
2022-09-21 10:22:33 +0530

    [sayali]Add:BubbleSort

---
+++ AlgorithmProgramm/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgramm
+{
+    internal class BubbleSort
+    {
+        public void Sort(string filePath)
+        {
+            int count = 0;
+            string data = File.ReadAllText(filePath);
+            string[] words = data.Split(",");
+            int[] arr = new int[words.Length];
+            for (int j = 0; j < words.Length; j++)
+            {
+                arr[j] = Convert.ToInt32(words[j]);
+            }
+            for (int i = 0; i <= words.Length - 2; i++)
+            {
+                for (int j = 0; j <= arr.Length - 2; j++)
+                {
+                    count = count + 1;
+                    if (arr[j] > (arr[j + 1]))
+                    {
+                        int temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("After Sorted Array is : ");
+            foreach (int a in arr)
+            {
+                Console.Write(a + " ");
+            }
+        }
+    }
+}
--- AlgorithmProgramm/Program.cs
+++ AlgorithmProgramm/Program.cs
@@ -8,14 +8,18 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter Your Choice : \n");
-                Console.WriteLine("1.Binary Search");
+                Console.WriteLine("\nEnter Your Choice : \n");
+                Console.WriteLine("1.Binary Search\n2.BubbleSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         BinarySearch search = new BinarySearch();
                         search.Search(FILE_PATH);
+                        break;
+                    case 2:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        bubbleSort.Sort(FILE_PATH);
                         break;
                     default:
                         Console.WriteLine("Invalid Choice");
